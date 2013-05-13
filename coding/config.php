e4c2b807fd8b4ba7395c0b117aa75699b5d95988
frevixzerobyte <frevixzerobyte@yahoo.com>
2013-05-13 01:12:20 -0600

    Update and rename user to config.php

---
+++ coding/config.php
@@ -0,0 +1,10 @@
+
+//untuk koneksi database
+<?php
+$server = "localhost";
+$user = "namahost";
+$pass = "passwordhost";
+$db = "databasekomprog";
+mysql_connect($server, $user, $pass);
+mysql_select_db($db);
+?>
